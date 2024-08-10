2fc42c62dae1f7e41ea7bd7310f8824227af396d
Jose Alvarez <jlam97@hotmail.es>
2024-08-10 11:47:44 -0600

    Agregamos a .gitignore

---
+++ HelloGit3.py
@@ -0,0 +1,9 @@
+x = int(input("Cual es el valor de x? "))
+z = int(input("Cual es el valor de y? "))
+
+valor = 50
+i = x + z
+if(i>valor):
+    print(f"El valor: {i}\nEs mayor a {valor}")
+else:
+    print(f"Tu valor escogido: {i}\nDebe ser mayor a {valor}")
